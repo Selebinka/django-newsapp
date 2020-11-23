d1ae7f119ffac2a7dbd68b45b406182b14a43f31
selebinka <oleg.selebinka@gmail.com>
2020-11-24 00:22:59 +0200

    added empty and null fields tolerances

--- news/models.py
+++ news/models.py
@@ -5,16 +5,16 @@ User = get_user_model()
 
 
 class Article(models.Model):
-    source = models.CharField(verbose_name='source', max_length=200)
-    author = models.CharField(verbose_name='author', max_length=200)
+    source = models.CharField(verbose_name='source', max_length=200, null=True, blank=True)
+    author = models.CharField(verbose_name='author', max_length=200, null=True, blank=True)
     publisher = models.ForeignKey(User, verbose_name='author', related_name='articles',
                                   on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(verbose_name='title', max_length=200)
-    description = models.TextField(verbose_name='description')
-    url = models.URLField(verbose_name='article url')
-    url_to_image = models.URLField(verbose_name='url to image')
-    published_at = models.DateTimeField(verbose_name='published at')
-    content = models.TextField(verbose_name='content')
+    title = models.CharField(verbose_name='title', max_length=200, null=True, blank=True)
+    description = models.TextField(verbose_name='description', null=True, blank=True)
+    url = models.URLField(verbose_name='article url', null=True, blank=True)
+    url_to_image = models.URLField(verbose_name='url to image', null=True, blank=True)
+    published_at = models.DateTimeField(verbose_name='published at', null=True, blank=True)
+    content = models.TextField(verbose_name='content', null=True, blank=True)
 
     class Meta:
         ordering = ('-published_at',)
