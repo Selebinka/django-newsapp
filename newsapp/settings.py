636a2ad55595e83cf9ec7a7bbbf0a37850be5ecb
selebinka <oleg.selebinka@gmail.com>
2020-11-24 02:29:21 +0200

    set postgresql db

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -100,8 +100,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
